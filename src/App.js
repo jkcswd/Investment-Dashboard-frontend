4bc11046355f949d09fe0494bdfa7d18e757074f
James Spencer <jameskcs1254@gmail.com>
2022-07-29 16:46:32 +0100

    Add test rendering of table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import useFetchData from './hooks/useFetchData';
 import './App.css';
+import Table from './components/Table';
 
-function App() {
+const App = () => {
+  const historicalData = useFetchData('aapl','2020-01-01', '2018-01-01');
+  const headings = ['date', 'open', 'high', 'low' , 'close', 'adjClose', 'volume'];
+
+
+  console.log(historicalData)
   return (
     <div className="App">
-
+      <Table headings={headings} body={historicalData} />
     </div>
   );
 }
