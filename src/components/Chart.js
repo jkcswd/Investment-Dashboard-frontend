890869f5a9e825b61cd58b23c399746eda7035f4
James Spencer <jameskcs1254@gmail.com>
2022-07-30 15:27:27 +0100

    delete all but presentational code

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,39 +1,9 @@
-import * as d3 from 'd3';
-import { useEffect, useRef} from 'react';
+import useD3 from '../hooks/useD3';
+import d3Func from '../d3Functions/d3Func';
 
 
 const Chart = (props) => {
-  const width = 500;
-  const height = 150;
-  const padding = 20;
-
-  const svgRef = useRef();
-  console.log(props)
-
-  useEffect(()=> {
-      const xScale = d3.scalePoint().domain(props.chartData.map(d => d.date)).range([(0+padding), (width-padding)]);
-      const yScale = d3.scaleLinear().domain([0, d3.max(props.chartData, d => d.adjClose)]).range([(height-padding), (0+padding)]);
-      const line = d3.line().x(d => xScale(d.date)).y(d => yScale(d.adjClose));
-      const xAxis = d3.axisBottom(xScale)
-      const yAxis = d3.axisLeft(yScale)
-
-      d3.select('#xaxis').remove()
-      d3.select(svgRef.current)
-         .append('g')
-         .attr('transform',`translate(0,${height - padding})`)
-         .attr('id','xaxis')
-         .call(xAxis)
-       
-     d3.select('#yaxis').remove()
-     d3.select(svgRef.current)
-         .append('g')
-         .attr('transform',`translate(${padding},0)`)
-         .attr('id','yaxis')
-         .call(yAxis)   
-  
-      d3.select(svgRef.current).select('path').attr('d',value => line(props.chartData)).attr('fill', 'none').attr('stroke', 'black');
-
-  },[props.chartData])
+  const svgRef = useD3(d3Func, props.chartData);
 
   return (
     <div className='Chart'>  
