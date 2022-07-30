9758f585d143a3f74573b2c08cf0da66a014c5bb
James Spencer <jameskcs1254@gmail.com>
2022-07-30 15:35:27 +0100

    pass func as prop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import useFetchData from './hooks/useFetchData';
 import './App.css';
 import Chart from './components/Chart';
+import d3Func from './d3Functions/d3Func';
 
 const App = () => {
   const historicalData = useFetchData('aapl'); 
-  console.log(historicalData)
 
   return (
     <div className="App">
       {historicalData &&
-        <Chart chartData={historicalData}/>
+        <Chart chartData={historicalData} d3Func={d3Func}/>
       }
     </div>
   );
