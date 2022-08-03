16555df547f9acbcc56bd7590c9c270c5b62428c
James Spencer <jameskcs1254@gmail.com>
2022-08-03 10:28:01 +0100

    Change to time scale

--- src/d3Functions/d3Func.js
+++ src/d3Functions/d3Func.js
@@ -1,31 +1,31 @@
 import * as d3 from 'd3';
 
-const d3Func = (data, ref) => {
-  const width = 500;
-  const height = 150;
-  const padding = 20;
-  const svgRef = ref;
-  const xScale = d3.scalePoint().domain(data.map(d => d.date)).range([(0+padding), (width-padding)]);
-  const yScale = d3.scaleLinear().domain([0, d3.max(data, d => d.adjClose)]).range([(height-padding), (0+padding)]);
-  const line = d3.line().x(d => xScale(d.date)).y(d => yScale(d.adjClose));
-  const xAxis = d3.axisBottom(xScale)
+const d3Func = (data, ref) => {//convert to date on x 
+  const width = 600;
+  const height = 300;
+  const padding = 50;
+  const xScale = d3.scaleTime().domain(d3.extent(data, d => new Date(d.date))).range([(0+padding), (width-padding)]);
+  const yScale = d3.scaleLinear().domain([-0.5, d3.max(data, d => d.adjClose)]).range([(height-padding), (0+padding)]);
+  const line = d3.line().x(d => xScale(new Date(d.date))).y(d => yScale(d.adjClose));
+  const xAxis = d3.axisBottom(xScale).ticks(10)
   const yAxis = d3.axisLeft(yScale)
 
   d3.select('#xaxis').remove()
-  d3.select(svgRef.current)
+  d3.select(ref.current)
       .append('g')
+      .style("font", "5px")
       .attr('transform',`translate(0,${height - padding})`)
       .attr('id','xaxis')
       .call(xAxis)
     
   d3.select('#yaxis').remove()
-  d3.select(svgRef.current)
+  d3.select(ref.current)
       .append('g')
       .attr('transform',`translate(${padding},0)`)
       .attr('id','yaxis')
       .call(yAxis)   
 
-  d3.select(svgRef.current).select('path').attr('d',value => line(data)).attr('fill', 'none').attr('stroke', 'black');
+  d3.select(ref.current).select('path').attr('d',value => line(data)).attr('fill', 'none').attr('stroke', 'black');
 }
 
 export default d3Func;
