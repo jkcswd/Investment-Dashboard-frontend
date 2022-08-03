663f367ba7f0dd41c8a72feae25ac5aa9a738a68
James Spencer <jameskcs1254@gmail.com>
2022-08-03 10:26:45 +0100

    Import css

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,11 +1,11 @@
 import useD3 from '../hooks/useD3';
-
+import '../css/Chart.css'
 const Chart = (props) => {
-  const svgRef = useD3(props.d3Func, props.chartData);
+  const svgRef = useD3(props.d3Func, props.chartData, props.ChartName);
 
   return (
     <div className='Chart'>  
-      <svg ref={svgRef} viewBox="0 0 500 150">
+      <svg ref={svgRef} viewBox="0 0 600 300">
         <path d="" fill="none" stroke="black" strokeWidth="0.5" />
       </svg>
     </div>
