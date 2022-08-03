e89dec46e23fea59337f75944d06b46ac7549514
James Spencer <jameskcs1254@gmail.com>
2022-08-03 20:55:38 +0100

    correct path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/styles.css';
+import './scss/styles.css';
 import App from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
