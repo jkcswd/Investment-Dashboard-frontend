c39027faaad7a0b63e0aa9eb2c08a68496c28f14
James Spencer <jameskcs1254@gmail.com>
2022-08-03 11:55:48 +0100

    add conditional rendering of charts or tables

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 import ChartDisplay from './components/ChartDisplay';
 import TableDisplay from './components/TableDisplay'
 
 const App = () => {
+  const [showChart, setShowChart] = useState(false);
+  const [showTable, setShowTable] = useState(false);
 
   return (
     <div className="App">
       <header><h1>Investment Dashboard</h1></header>
       <main>
-        <ChartDisplay/>
-        <TableDisplay/>
+        {!showChart && !showTable &&
+          <button onClick={() => setShowChart(true)}>Charts</button>
+        }
+        {!showChart && !showTable &&
+          <button onClick={() => setShowTable(true)}>Tables</button>
+        }
+        {showChart &&
+          <ChartDisplay/>
+        }
+        { showTable &&
+          <TableDisplay/>
+        }
       </main>
       <footer>Created by James Spencer</footer>
     </div>
