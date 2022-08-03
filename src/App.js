efde9d8f8e6d2135040bc20ab1ba49dadc15bd57
James Spencer <jameskcs1254@gmail.com>
2022-08-03 11:38:11 +0100

    split chart container into new component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import useFetchData from './hooks/useFetchData';
 import './App.css';
-import Chart from './components/Chart';
-import d3Func from './d3Functions/d3Func';
+import ChartDisplay from './components/chartDispaly';
+
 
 const App = () => {
-  const historicalData = useFetchData('aapl'); 
 
   return (
     <div className="App">
-      {historicalData &&
-        <Chart chartData={historicalData} d3Func={d3Func}/>
-      }
+      <header><h1>Investment Dashboard</h1></header>
+      <main>
+        <ChartDisplay/>
+      </main>
+      <footer>Created by James Spencer</footer>
     </div>
   );
 }
---
+++ src/components/chartDispaly.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import Chart from '../components/Chart';
+import d3Func from '../d3Functions/d3Func'
+import fetchData from '../apiFunctions/fetchData'
+
+const ChartDisplay = () => {
+  const [tickerSymbol, setTickerSymbol] = useState('');
+  const [dateTo, setDateTo] = useState('');
+  const [dateFrom, setDateFrom] = useState('');
+  const [data, setData] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const apiData = await fetchData(tickerSymbol, dateTo, dateFrom)
+
+    setData(apiData);
+  }
+
+  return (
+    <div className="ChartDisplay">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="ticker">Ticker Symbol:</label>
+          <input id="ticker" type="text" value={tickerSymbol} onChange={ e => setTickerSymbol(e.target.value) } />
+          <label htmlFor="dateTo">date to:</label>
+          <input id="dateTo" type="date" value={dateTo} onChange={ e => setDateTo(e.target.value) } />
+          <label htmlFor="dateFrom">date from:</label>
+          <input id="dateFrom" type="date" value={dateFrom} onChange={ e => setDateFrom(e.target.value) } />
+          <input type="submit"/>
+        </form>
+        { data && 
+          <Chart chartData={data} d3Func={d3Func}/>
+        }
+    </div>
+  )
+}
+
+export default ChartDisplay;
