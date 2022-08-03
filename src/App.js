21613cbadcca5ecc852eaaf037acaa4a02573a37
James Spencer <jameskcs1254@gmail.com>
2022-08-03 11:46:56 +0100

    Add table container component to JSX

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import ChartDisplay from './components/ChartDisplay';
+import TableDisplay from './components/TableDisplay'
 
 const App = () => {
 
@@ -8,6 +9,7 @@ const App = () => {
       <header><h1>Investment Dashboard</h1></header>
       <main>
         <ChartDisplay/>
+        <TableDisplay/>
       </main>
       <footer>Created by James Spencer</footer>
     </div>
