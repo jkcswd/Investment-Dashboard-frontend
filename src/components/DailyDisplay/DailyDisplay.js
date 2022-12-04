9c9dd182f98aeb7a86f974accc612083c186ccdf
James Spencer <jameskcs1254@gmail.com>
2022-12-04 15:27:58 +0000

    add fetch data into useffect

--- src/components/DailyDisplay/DailyDisplay.js
+++ src/components/DailyDisplay/DailyDisplay.js
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import fetchReport from "../../apiFunctions/fetchReport";
 import Table from '../Table/Table'
 
 
 const DailyDisplay = () => {
   const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchReport();
+      console.log(data)
+      setData(data);
+    }
+    
+    fetchData();
+  });
 
   return (
     <div className="DailyDisplay">
