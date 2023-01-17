61b6a344a46cf18c6ba3584dcc4a27dbcdf46d04
James Spencer <jameskcs1254@gmail.com>
2023-01-17 12:42:50 +0000

    create outline of component

--- src/components/PortfolioTracker/PortfolioTracker.js
+++ src/components/PortfolioTracker/PortfolioTracker.js
@@ -1,5 +1,31 @@
-/* 
+import { useEffect, useState } from "react";
+import Table from '../Table/Table'
+import fetchPortfolio from "../../apiFunctions/fetchPortfolio";
 
 
+const PortfolioTracker = () => {
+  const [data, setData] = useState(null);
 
-*/
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchPortfolio();
+      console.log(data)
+      setData(data);
+    }
+    
+    fetchData();
+  });
+
+  return (
+    <div className="DailyDisplay">
+        { data && 
+          <Table data={data}/>
+        }
+        { !data && 
+          <p>No portfolio found</p>
+        }
+    </div>
+  )
+}
+
+export default DailyDisplay;
