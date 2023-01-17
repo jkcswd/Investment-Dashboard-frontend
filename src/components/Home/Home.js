0b0ed99c7589234dcb9d86720bb59115cd3fec27
James Spencer <jameskcs1254@gmail.com>
2023-01-17 13:49:42 +0000

    Create home page component and styling

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import './Home.css'
+
+const Home = () => {
+
+  return (
+    <div className='Home'>  
+      <p>Welcome To Your Integrated Investment Analysis and Portfolio Management Application</p>
+    </div>
+  );
+}
+
+export default Home;
