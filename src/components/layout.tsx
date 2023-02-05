2f8ae8af1fb0bf5e76b6dbd12adba5e444691bc9
James Spencer <jameskcs1254@gmail.com>
2023-02-05 08:40:48 +0000

    create basic layout file

---
+++ src/components/layout.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Layout( children : React.ReactNode) {
+  return (
+    <div>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+}
