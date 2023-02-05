4f0ab2ce9e3ebb146f3a7c3ea3c6a1b76b0fafc2
James Spencer <jameskcs1254@gmail.com>
2023-02-05 09:53:16 +0000

    remove main as already in layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,9 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
         <p>Welcome to your integrated investment management and analysis dashboard</p>
-      </main>
     </Layout>
   )
 }
