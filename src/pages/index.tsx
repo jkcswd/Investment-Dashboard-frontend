f24270db928ffd2414432f29055b1bdb8b4fa91f
James Spencer <jameskcs1254@gmail.com>
2023-02-05 09:55:43 +0000

    add text style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <p>Welcome to your integrated investment management and analysis dashboard</p>
+        <p className={styles.mainText}>Welcome to your integrated investment management and analysis dashboard</p>
     </Layout>
   )
 }
