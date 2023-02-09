54b6c160aa953a0097861ac842ac9174e37042e0
James Spencer <jameskcs1254@gmail.com>
2023-02-09 07:41:00 +0000

    create file and boilerplate code

---
+++ src/pages/portfolio-tracker.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Layout from '@/components/Layout'
+
+export default function PortfolioTracker() {
+  return (
+    <Layout>
+      <Head>
+        <title>InDash - Portfolio Tracker</title>
+      </Head>
+        <p>TRACK</p>
+    </Layout>
+  )
+}
