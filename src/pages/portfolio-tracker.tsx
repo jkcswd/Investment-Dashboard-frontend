a5cc48c03cf38ffa076b6f241aaf398d5d670c63
James Spencer <jameskcs1254@gmail.com>
2023-02-10 15:47:08 +0000

    add links

--- src/pages/portfolio-tracker.tsx
+++ src/pages/portfolio-tracker.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Link from 'next/link';
+import styles from '@/styles/PortfolioTracker.module.css'
 import Layout from '@/components/Layout'
 
 export default function PortfolioTracker() {
@@ -7,7 +9,10 @@ export default function PortfolioTracker() {
       <Head>
         <title>InDash - Portfolio Tracker</title>
       </Head>
-        <nav aria-label='portfolio-nav'></nav>
+        <nav className={styles.nav} aria-label='portfolio-nav'>
+          <Link className={styles.navItem} href='trade-input'>Add Trade Enty/Exit</Link>
+          <Link className={styles.navItem} href='adjustment-input'>Add Portfolio Adjustment</Link>
+        </nav>
     </Layout>
   )
 }
