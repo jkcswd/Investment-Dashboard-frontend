aeafacbb9de358feb54757a824b6484259dfdb6e
James Spencer <jameskcs1254@gmail.com>
2023-02-13 11:52:04 +0000

    create basic user POST?Get API

--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -1,26 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import dbConnect from '@/lib/dbConnect'
-import { ResponseFuncs, User } from '@/lib/types'
+import { ResponseFuncs } from '@/lib/types'
 import User from '@/lib/models/User';
 
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const method: keyof ResponseFuncs = req.method as keyof ResponseFuncs;
-  const errorHandler = (error: Error) => res.status(400).json({ error }); {
+  const errorHandler = (error: Error) => res.status(400).json({ error });
   
   const handleCase: ResponseFuncs = {
     GET: async (req: NextApiRequest, res: NextApiResponse) => {
-      const connection = await dbConnect() 
+      try {
+        await dbConnect();
+        const users = await User.find({});
+
+        res.json(users);
+      } catch (error: any) {
+        errorHandler(error);
+      }
     },
 
     POST: async (req: NextApiRequest, res: NextApiResponse) => {
-      const connection = await dbConnect() 
+      try {
+        await dbConnect();
+        const user = new User(req.body);
+        await user.save();
+
+        res.json(user);
+      } catch (error : any) {
+        errorHandler(error);
+      }
     },
   }
 
-
-  res.status(200).json({ userName: 'John Doe', email:'a@gmail.com' })
+  const response = handleCase[method];
+  if (response) response(req, res);
+  else res.status(400).json({ error: "No Response" });
 }
