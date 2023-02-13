89a53ead7627710110a9404a8b2b2e4c5c3741e0
James Spencer <jameskcs1254@gmail.com>
2023-02-13 11:28:53 +0000

    delete unused interface

--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -1,11 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import dbConnect from '@/lib/dbConnect'
 import { ResponseFuncs, User } from '@/lib/types'
+import User from '@/lib/models/User';
 
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<User>
+  res: NextApiResponse
 ) {
+  const method: keyof ResponseFuncs = req.method as keyof ResponseFuncs;
+  const errorHandler = (error: Error) => res.status(400).json({ error }); {
+  
+  const handleCase: ResponseFuncs = {
+    GET: async (req: NextApiRequest, res: NextApiResponse) => {
+      const connection = await dbConnect() 
+    },
+
+    POST: async (req: NextApiRequest, res: NextApiResponse) => {
+      const connection = await dbConnect() 
+    },
+  }
+
+
   res.status(200).json({ userName: 'John Doe', email:'a@gmail.com' })
 }
